6292f4ab53ba2999bce3a435b8f45ae89d3fa2c3
Kay Wolter <kay.wolter@funktionswerk.de>
2018-04-04 13:41:35 +0200

    Integrated PHP client

--- loghero.php
+++ loghero.php
@@ -5,13 +5,72 @@ Version: 0.0.1
 */
 
 if ( !class_exists( 'LogHeroClient_Plugin' ) ) {
+    require_once(dirname(__FILE__) . '/sdk/src/LogHero.php');
+
+    # TODO Handle proxies HTTP_X_FORWARDED_FOR (see cerber)
+    class LogHeroLogEventFactory {
+        private $ipAddressKeys = array(
+            'REMOTE_ADDR',
+            'HTTP_X_REAL_IP',
+            'HTTP_CLIENT_IP',
+            'SERVER_ADDR'
+        );
+
+        public function create() {
+            $logEvent = new LHLogEvent();
+            $this->setLandingPagePath($logEvent);
+            $this->setUserAgent($logEvent);
+            $this->setIpAddress($logEvent);
+            $this->setTimestamp($logEvent);
+            return $logEvent;
+        }
+
+        private function setLandingPagePath($logEvent) {
+            $logEvent->setUserAgent($_SERVER['REQUEST_URI']);
+        }
+
+        private function setUserAgent($logEvent) {
+            if (!empty($_SERVER['HTTP_USER_AGENT'])) {
+                $ua = $_SERVER['HTTP_USER_AGENT'];
+                $logEvent->setUserAgent($ua);
+            }
+        }
+
+        private function setTimestamp($logEvent) {
+            $unixTimestamp = null;
+            if (!empty( $_SERVER['REQUEST_TIME_FLOAT'])) { // PHP >= 5.4
+                $unixTimestamp = $_SERVER['REQUEST_TIME_FLOAT'];
+            }
+            else {
+                $unixTimestamp = microtime( true );
+            }
+            $timeStamp = new DateTime();
+            $timeStamp->setTimestamp($unixTimestamp);
+            $logEvent->setTimestamp($timeStamp);
+        }
+
+        private function setIpAddress($logEvent) {
+            $ipAddress = null;
+            foreach ($this->ipAddressKeys as $key) {
+                $ipAddress = filter_var($_SERVER[$key], FILTER_VALIDATE_IP);
+                if ($ipAddress) {
+                    break;
+                }
+            }
+            $logEvent->setIpAddress($ipAddress);
+        }
+
+    }
 
     class LogHeroClient_Plugin {
         private static $Instance = false;
-        private $apiKey;
+        private $apiClient;
+        private $logEventFactory;
 
         public function __construct() {
-            $this->apiKey = get_option('api_key');
+            $apiKey = get_option('api_key');
+            $this->logEventFactory = new LogHeroLogEventFactory();
+            $this->apiClient = new LHClient($apiKey, 3);
             add_action('shutdown', array($this, 'sendLogEvent'));
         }
 
@@ -23,7 +82,9 @@ if ( !class_exists( 'LogHeroClient_Plugin' ) ) {
         }
 
         public function sendLogEvent() {
-            echo '<p>SENDING LOG EVENT WITH API KEY '.$this->apiKey.'</p>';
+            $logEvent = $this->logEventFactory->create();
+            $this->apiClient->submit($logEvent);
+            $this->apiClient->flush();
         }
 
     }
