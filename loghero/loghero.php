10925df2e9314c03df3069af367a1bec4be683e3
Kay Wolter <kay.wolter@funktionswerk.de>
2018-05-22 11:31:08 +0200

    LOG-78 Added MIT license info

--- loghero/loghero.php
+++ loghero/loghero.php
@@ -1,7 +1,32 @@
 <?php
 /*
 Plugin Name: LogHero Client
-Version: 0.0.1
+Version:     0.1.1
+Description: The official PHP Wordpress plugin for loghero.io.
+Author:      Kay Wolter
+Author URI:  https://www.funktionswerk.de/
+License:     MIT
+
+Copyright (c) 2018 Cross Platform Solutions GmbH
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
 */
 
 namespace LogHero\Wordpress;
@@ -18,7 +43,7 @@ if ( !class_exists( 'LogHeroClient_Plugin' ) ) {
         protected $apiKey;
         protected $apiClient;
         protected $logEventFactory;
-        protected $clientId = 'Wordpress Plugin loghero/wp@0.1.0';
+        protected $clientId = 'Wordpress Plugin loghero/wp@0.1.1';
 
         public function __construct() {
             $this->apiKey = get_option('api_key');
