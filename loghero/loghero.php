a48cdafd4b6d35e87d1299bf7eadf94545f4e89a
Kay Wolter <kay.wolter@funktionswerk.de>
2018-06-05 10:59:15 +0200

    LOG-76 Updated plugin to use refactored log transports

--- loghero/loghero.php
+++ loghero/loghero.php
@@ -34,27 +34,24 @@ namespace LogHero\Wordpress;
 use LogHero\Client\FileLogBuffer;
 
 if ( !class_exists( 'LogHeroClient_Plugin' ) ) {
-
-    require_once(dirname(__FILE__) . '/sdk/src/LogHero.php');
-    require_once(dirname(__FILE__) . '/sdk/src/LogBuffer.php');
-    require_once(dirname(__FILE__) . '/sdk/src/LogEventFactory.php');
-    require_once(dirname(__FILE__) . '/sdk/src/APIAccess.php');
+    require_once __DIR__ . '/sdk/src/event/LogEventFactory.php';
+    require_once __DIR__ . '/sdk/src/buffer/FileLogBuffer.php';
+    require_once __DIR__ . '/sdk/src/http/APIAccess.php';
+    require_once __DIR__ . '/sdk/src/transport/LogTransport.php';
 
     class LogHeroClient_Plugin {
         protected static $Instance = false;
         protected $apiKey;
-        protected $apiClient;
+        protected $logTransport;
         protected $logEventFactory;
         protected $clientId = 'Wordpress Plugin loghero/wp@0.1.2';
 
         public function __construct() {
             $this->apiKey = get_option('api_key');
             $this->logEventFactory = new \LogHero\Client\LogEventFactory();
-            $this->apiClient = \LogHero\Client\Client::create(
-                $this->apiKey,
-                $this->clientId,
-                new FileLogBuffer(__DIR__ . '/logs/buffer.loghero.io.txt')
-            );
+            $logBuffer = new \LogHero\Client\FileLogBuffer(__DIR__ . '/logs/buffer.loghero.io.txt');
+            $apiAccess = new \LogHero\Client\APIAccess($this->apiKey, $this->clientId);
+            $this->logTransport = new \LogHero\Client\LogTransport($logBuffer, $apiAccess);
             add_action('shutdown', array($this, 'sendLogEvent'));
         }
 
@@ -71,10 +68,7 @@ if ( !class_exists( 'LogHeroClient_Plugin' ) ) {
             if ($logEvent->getUserAgent() == $this->clientId) {
                 return;
             }
-            $this->apiClient->submit($logEvent);
-            if ($this->apiClient->needsFlush()) {
-                $this->triggerFlush();
-            }
+            $this->logTransport->submit($logEvent);
         }
 
         # TODO Test this function
@@ -110,6 +104,6 @@ if ( !class_exists( 'LogHeroClient_Plugin' ) ) {
     LogHeroClient_Plugin::getInstance();
 
     if (is_admin()) {
-        require_once(dirname(__FILE__) . '/admin/loghero-admin.php');
+        require_once(__DIR__ . '/admin/loghero-admin.php');
     }
 }
