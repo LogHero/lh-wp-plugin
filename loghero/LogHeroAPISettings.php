b112f29fe3592925e837455df2baca235541f19d
Kay Wolter <kay.wolter@funktionswerk.de>
2018-06-15 10:40:21 +0200

    LOG-90 Persist dev API settings by writing them to a JSON file

--- loghero/LogHeroAPISettings.php
+++ loghero/LogHeroAPISettings.php
@@ -8,13 +8,15 @@ class LogHeroAPISettings extends APISettingsDefault {
 
     public function __construct($apiDevSettingsFile = null) {
         if (!$apiDevSettingsFile) {
-            $apiDevSettingsFile = __DIR__ . '/logs/dev.loghero.io.txt';
+            $apiDevSettingsFile = __DIR__ . '/logs/dev.loghero.io.json';
         }
         $this->apiDevSettingsFile = $apiDevSettingsFile;
+        $this->initializeFromDevSettingsFile();
     }
 
     public function setAPILogPackageEndpoint($apiLogPackageEndpoint) {
         $this->apiLogPackageEndpoint = $apiLogPackageEndpoint;
+        $this->refreshDevSettingsFile();
     }
 
     public function getAPILogPackageEndpoint() {
@@ -22,5 +24,26 @@ class LogHeroAPISettings extends APISettingsDefault {
             return $this->apiLogPackageEndpoint;
         }
         return parent::getAPILogPackageEndpoint();
+    }
+
+    private function refreshDevSettingsFile() {
+        $jsonData = array(
+            'apiLogPackageEndpoint' => $this->apiLogPackageEndpoint
+        );
+        file_put_contents($this->apiDevSettingsFile, json_encode($jsonData));
+    }
+
+    private function initializeFromDevSettingsFile() {
+        if (!file_exists($this->apiDevSettingsFile)) {
+            return;
+        }
+        $jsonString = file_get_contents($this->apiDevSettingsFile);
+        $json = json_decode($jsonString, true);
+        if (!$json) {
+            return;
+        }
+        if (array_key_exists('apiLogPackageEndpoint', $json)) {
+            $this->apiLogPackageEndpoint = $json['apiLogPackageEndpoint'];
+        }
     }
 }
--- loghero/tests/test-LogHeroAPISettings.php
+++ loghero/tests/test-LogHeroAPISettings.php
@@ -5,7 +5,7 @@ class LogHeroAPISettingsTest extends \WP_UnitTestCase {
     private $devSettingsFilename;
 
     public function setUp() {
-        $this->devSettingsFilename = __DIR__ . '/logs/dev.loghero.io.txt';
+        $this->devSettingsFilename = __DIR__ . '/logs/dev.loghero.io.json';
     }
 
     public function tearDown() {
@@ -23,5 +23,31 @@ class LogHeroAPISettingsTest extends \WP_UnitTestCase {
         $settings = new LogHeroAPISettings($this->devSettingsFilename);
         $settings->setAPILogPackageEndpoint('https://test.loghero.io/logs/');
         static::assertEquals('https://test.loghero.io/logs/', $settings->getAPILogPackageEndpoint());
+    }
+
+    public function testPersistCustomizedSettingsInDevSettingsFile() {
+        $settings = new LogHeroAPISettings($this->devSettingsFilename);
+        $settings->setAPILogPackageEndpoint('https://test.loghero.io/logs/');
+        $this->assertEndpointInDevSettingsFile('https://test.loghero.io/logs/');
+        $settingsReinstantiated = new LogHeroAPISettings($this->devSettingsFilename);
+        static::assertEquals('https://test.loghero.io/logs/', $settingsReinstantiated->getAPILogPackageEndpoint());
+    }
+
+    public function testHandleParseErrors() {
+        file_put_contents($this->devSettingsFilename, 'NO_JSON');
+        $settings = new LogHeroAPISettings($this->devSettingsFilename);
+        static::assertEquals('https://api.loghero.io/logs/', $settings->getAPILogPackageEndpoint());
+    }
+
+    public function testHandleEmptyJson() {
+        file_put_contents($this->devSettingsFilename, '{}');
+        $settings = new LogHeroAPISettings($this->devSettingsFilename);
+        static::assertEquals('https://api.loghero.io/logs/', $settings->getAPILogPackageEndpoint());
+    }
+
+    private function assertEndpointInDevSettingsFile($expectedEndpoint) {
+        $jsonString = file_get_contents($this->devSettingsFilename);
+        $json = json_decode($jsonString, true);
+        static::assertEquals($json['apiLogPackageEndpoint'], $expectedEndpoint);
     }
 }
