56c91cfaebca5ce6ad168bdac66b28f7188f7cc6
Kay Wolter <kay.wolter@funktionswerk.de>
2018-07-23 12:07:45 +0200

    LOG-136 Check if get_option is available before trying to read settings from DB

--- loghero/LogHeroPluginSettings.php
+++ loghero/LogHeroPluginSettings.php
@@ -7,7 +7,11 @@ use \LogHero\Client\LogTransportType;
 class LogHeroPluginSettings {
 
     public static function getTransportType() {
-        $useSyncTransport = get_option('use_sync_transport');
+        $useSyncTransport = null;
+        // TODO: This needs testing
+        if (function_exists('get_option')) {
+            $useSyncTransport = get_option('use_sync_transport');
+        }
         if ($useSyncTransport) {
             return LogTransportType::Sync;
         }
